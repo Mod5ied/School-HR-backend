d70777e54665dd6136e9fb72c3f35e6bf4c92d33
Ogwuru Patrick <sirpatrickchigozie@gmail.com>
2023-04-29 02:14:35 +0100

    opted for encryption for accesstokens over hashing.. refactored encryption services to fit needs...

--- src/services/encrypt/tokens.encrypt.ts
+++ src/services/encrypt/tokens.encrypt.ts
@@ -1,12 +1,36 @@
-import * as bcrypt from 'bcrypt';
+// import { RefreshToken } from "../tokens/models/refreshtokens.model";
+import { AccessToken } from "../tokens/models/accesstokens.model";
+import { REFRESH_SECRET } from "../tokens/tokens.secrets";
+import { IToken } from "../tokens/tokens.types";
+import { InjectModel } from "@nestjs/mongoose";
+import { Injectable } from "@nestjs/common"
+import { Model } from "mongoose";
+import CryptoJS from "crypto-js";
 
-export async function encryptToken(token: string) {
-  const salt = await bcrypt.genSalt(15);
-  return await bcrypt.hash(token, salt);
-}
+@Injectable()
+export class CryptService {
+  constructor(
+    @InjectModel(AccessToken.name)
+    private readonly accessTokens: Model<IToken>,
+    // @InjectModel(RefreshToken.name)
+    // private readonly refreshTokens: Model<IToken>
+  ) { }
 
-export async function compareTokens(token: string, hashedToken: string) {
-  const isMatch = await bcrypt.compare(token, hashedToken);
-  if (!isMatch) return { ok: false };
-  return { ok: true };
+  /** Returns token encrypted with aes-256 algo. */
+  public async encryptToken(token: string) {
+    /* encrypt the jwt return */
+    const encrypted = CryptoJS.AES.encrypt(token, REFRESH_SECRET);
+    return encrypted.toString();
+  }
+
+  /** Returns decrypted tokens. */
+  public async decryptTokens(encryptedToken: string, email: string) {
+    /* decrypts the token and returns true if it matches unhashed token */
+    const accessDoc = await this.accessTokens.findOne({ tokenEmail: email }).lean();
+    if (!accessDoc) throw new Error('Token is expired!');
+
+    const decrypted = CryptoJS.AES.decrypt(encryptedToken, REFRESH_SECRET).toString();
+    if (decrypted === accessDoc.token) return { match: true, doc: accessDoc };
+    return { match: false };
+  }
 }
--- src/services/tokens/tokens.service.ts
+++ src/services/tokens/tokens.service.ts
@@ -1,8 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ACCESS_SECRET, LINK, REFRESH_SECRET, SECRET } from './tokens.secrets';
-import { compareTokens, encryptToken } from '../encrypt/tokens.encrypt';
 import { ResponseService } from '../broadcast/response/reponse.tokens';
 import { RefreshToken } from './models/refreshtokens.model';
 import { AccessToken } from './models/accesstokens.model';
+import { CryptService } from '../encrypt/tokens.encrypt';
 import { ErrorService } from '../errors/error.service';
 import { IToken, Users } from './tokens.types';
 import { InjectModel } from '@nestjs/mongoose';
@@ -15,9 +16,11 @@ export class TokenService {
   constructor(
     private readonly responseService: ResponseService,
 
-    private readonly jwtService: JwtService,
+    private readonly cryptService: CryptService,
 
     private readonly errorService: ErrorService,
+
+    private readonly jwtService: JwtService,
 
     @InjectModel(AccessToken.name)
     private readonly accessToken: Model<IToken>,
@@ -26,103 +29,89 @@ export class TokenService {
     private readonly refreshToken: Model<IToken>,
   ) { }
 
-  /** create token doc (and exec self-destruct for doc on creation) for the user. */
-  private async generateTokenDoc(
-    model: Model<IToken>,
-    user: Partial<Users>,
-    token,
-  ) {
-    try {
-      const { role, email, permissions } = user;
-      return await model.create({
-        token,
-        tokenEmail: email,
-        tokenPermissions: permissions,
-        role: role,
-      });
-    } catch (e) {
-      return this.errorService.badRequest(e.message);
-    }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  private buildVerificationLink(email: string, hashedToken: string): string {
+    return `\${LINK}verify/?token=\${hashedToken}/?email=\${email}`;
   }
 
+  private buildEmailContent(vLink: string): string {
+    return `
+      <form action="\\${vLink}" method="POST">
+        <button type="submit" onclick="verify()">Go to homepage</button>
+      </form>
+    `;
+  }
+
+  /** create token doc (and exec self-destruct for doc on creation) for the user. */
+  private async generateTokenDoc(model: Model<IToken>, user: Partial<Users>, token: string) {
+    const { role, email, permissions } = user;
+    return await model.create({
+      token,
+      tokenEmail: email,
+      tokenPermissions: permissions,
+      role: role,
+    }).catch(error => {
+      return this.errorService.badRequest(`AccessToken-Doc creation failed! - ${error.message}`);
+    })
+  }
+
+  /** returns an encrypted jwt and the original saved to an access-token doc.  */
   private async generateAccessToken(user: Partial<Users>) {
     const { email, role, permissions } = user;
-    try {
-      const aToken = await this.jwtService.signAsync(
-        { email, role, permissions },
-        {
-          expiresIn: '4h',
-          issuer: `${role}-issuer`,
-          secret: ACCESS_SECRET,
-        },
-      );
-      await this.generateTokenDoc(this.accessToken, user, aToken);
-      return aToken;
-    } catch (e) {
-      this.errorService.badRequest(
-        `Failed to create AccessToken_Doc, where error - ${e.message}`,
-      );
-    }
+    const accessToken = await this.jwtService.signAsync(
+      { email, role, permissions },
+      {
+        expiresIn: '4h',
+        issuer: `${role}-issuer`,
+        secret: ACCESS_SECRET,
+      },
+    ).catch(error => {
+      throw new Error(`AccessToken creation failed! - ${error.message}`);
+    })
+    await this.generateTokenDoc(this.accessToken, user, accessToken);
+    return this.cryptService.encryptToken(accessToken);
   }
 
+  /** generates a jwt and creates a ref-token doc using the token.  */
   private async generateRefreshToken(user: Partial<Users>) {
     const { email, role, permissions } = user;
-    try {
-      const rToken = await this.jwtService.signAsync(
-        { email, role, permissions },
-        {
-          issuer: `${role}-issuer`,
-          secret: REFRESH_SECRET,
-        },
-      );
-      await this.generateTokenDoc(this.refreshToken, user, rToken);
-      return rToken;
-    } catch (e) {
-      this.errorService.badRequest(
-        `Failed to create RefreshToken_Doc, where error - ${e.message}`,
-      );
-    }
+    const refreshToken = await this.jwtService.signAsync(
+      { email, role, permissions },
+      {
+        issuer: `${role}-issuer`,
+        secret: REFRESH_SECRET,
+      },
+    ).catch(error => {
+      throw new Error(`RefreshToken creation failed! - ${error.message}`);
+    })
+    await this.generateTokenDoc(this.refreshToken, user, refreshToken);
   }
 
   public async generateTokens({ email, permissions, role }) {
-    try {
-      const accessToken = await this.generateAccessToken({
-        email,
-        role,
-        permissions,
-      });
-      const hashedToken = await encryptToken(accessToken);
-      await this.generateRefreshToken({ email, role, permissions });
-      const vLink = `${LINK}verify/?token=${hashedToken}/?email=${email}`;
+    const hashedToken = await this.generateAccessToken({ email, permissions, role });
+    await this.generateRefreshToken({ email, permissions, role });
+    const vLink = this.buildVerificationLink(email, hashedToken);
+    const emailContent = this.buildEmailContent(vLink);
 
-      const emailContent = `
-      <form action="\${vLink}" method="POST">
-        <button type="submit" onclick="verify()">Go to homepage</button>
-      </form>
-      `;
-      return this.responseService.respondToEmail(hashedToken, {
-        to: email,
-        from: SECRET.email,
-        subject: SECRET.subject,
-        html: emailContent
-      });
-    } catch (jwtErr) {
-      return this.errorService.badRequest(jwtErr.message);
-    }
+    return this.responseService.respondToEmail(hashedToken, {
+      to: email,
+      from: SECRET.email,
+      subject: SECRET.subject,
+      html: emailContent
+    }).catch(error => {
+      return this.errorService.badRequest(error.message);
+    })
   }
 
   public async runVerifyAccessToken(token: string, email: string) {
-    try {
-      const tokenDoc = await this.accessToken.findOne({ tokenEmail: email }).lean()
-      if (!tokenDoc) return this.errorService.unauthorizedRequest('Token is expired!')
 
-      const validToken = await compareTokens(tokenDoc.token, token);
-      if (validToken.ok)
-        return this.responseService.respondToClient(token, { role: tokenDoc.role, permissions: tokenDoc.tokenPermissions });
+    const validToken = await this.cryptService.decryptTokens(token, email);
+    if (validToken.match)
+      return this.responseService.respondToClient(token, {
+        role: validToken.doc.role, permissions: validToken.doc.tokenPermissions
+      });
 
-      return this.errorService.unauthorizedRequest('Token is invalid');
-    } catch (e) {
-      return this.errorService.badRequest(e.message);
-    }
+    return this.errorService.unauthorizedRequest('Token verification failed - Invalid token');
   }
 }
